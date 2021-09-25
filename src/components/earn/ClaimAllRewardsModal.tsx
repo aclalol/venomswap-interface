846748bf327762af49af19786afffe5e850bf756
Hepa Finance <86351104+hepafinance@users.noreply.github.com>
2021-09-25 06:40:10 +0300

    Update ClaimAllRewardsModal.tsx

--- src/components/earn/ClaimAllRewardsModal.tsx
+++ src/components/earn/ClaimAllRewardsModal.tsx
@@ -100,8 +100,7 @@ export default function ClaimAllRewardsModal({ isOpen, onDismiss, stakingInfos }
             </AutoColumn>
           )}
           <TYPE.subHeader style={{ textAlign: 'center' }}>
-            The sum above includes both locked and unlocked {govToken?.symbol} rewards across all the pools you are
-            staking in.
+            {govToken?.symbol} rewards across all the pools you are staking in.
           </TYPE.subHeader>
           <ButtonError disabled={!!error} error={!!error} onClick={onClaimReward}>
             {error ?? 'Claim'}
